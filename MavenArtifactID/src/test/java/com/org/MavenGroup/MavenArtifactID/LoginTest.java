9f856d1ca997225ee4f816d2d37c4ac07c1b2a1f
kaiva <kaiva@DESKTOP-Q25CNSK.CYCLOTRONGROUP.COM>
2019-09-06 15:53:20 +0530

    First Commit

---
+++ MavenArtifactID/src/test/java/com/org/MavenGroup/MavenArtifactID/LoginTest.java
@@ -0,0 +1,50 @@
+package com.org.MavenGroup.MavenArtifactID;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	@Test
+	public void TC1_Login()
+	//public static void main(String[] args)
+	{
+		System.out.println("pwd typed");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaiva\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+
+		driver.get("https://teamswebstaging.azurewebsites.net");
+		
+		driver.manage().window().maximize();
+	
+			
+			driver.findElement(By.id("i0116")).sendKeys("kaivalya@cyclotrondev.com");
+			driver.findElement(By.id("idSIButton9")).click();
+		
+		
+			driver.findElement(By.id("i0118")).sendKeys("Metro@may");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			driver.findElement(By.id("idSIButton9")).click();
+		
+	
+			driver.findElement(By.id("idSIButton9")).click();
+			//System.out.println("pwd typed");
+		
+			// test
+			 String actualTitle = driver.getTitle();
+						 
+			 String expectedTitle = "Teams Hub";
+			 if(actualTitle.equalsIgnoreCase(expectedTitle))
+			 System.out.println("Title Matched - Test Case Passed");
+			 else
+			 System.out.println("Title didn't match - Test Case Failed");
+	}
+}
